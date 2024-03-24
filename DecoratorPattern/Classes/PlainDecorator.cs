595661d3d8210bfb66fa20f07f73ee6dfa6cee8f
Neerav Panchal <neeravpanchal26@gmail.com>
2024-03-24 14:28:29 +0000

    Define a decorator named PlainDecorator that inherits from the Decorator<string> abstract class. The GetText() method should prefix the text of the base class with some text of your choosing.

---
+++ DecoratorPattern/Classes/PlainDecorator.cs
@@ -0,0 +1,11 @@
+public class PlainDecorator : Decorator<string>
+{
+    public PlainDecorator(IComponent<string> component) : base(component)
+    {
+    }
+
+    public override string GetText()
+    {
+        return "Neerav says: " + base.GetText();
+    }
+}
