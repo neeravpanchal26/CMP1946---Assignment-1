edb223c8fbf442ecb325b0ab234350e13ec2f8bd
Neerav Panchal <neeravpanchal26@gmail.com>
2024-03-24 14:35:06 +0000

    in your Main method, create a ConcreteComponent and one of each decorator. Call the GetText() method on each object and print the result to the console.

--- DecoratorPattern/Program.cs
+++ DecoratorPattern/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿IComponent<string> component = new ConcreteComponent();
+IComponent<string> colorDecorator = new ColorDecorator(component);
+IComponent<string> plainDecorator = new PlainDecorator(component);
+IComponent<string> upperCaseDecorator = new UpperCaseDecorator(component);
+
+Console.WriteLine(component.GetText());
+Console.WriteLine(colorDecorator.GetText());
+Console.WriteLine(plainDecorator.GetText());
+Console.WriteLine(upperCaseDecorator.GetText());
